224c86de31c31e0f80e5fb6544ab001878191075
Mihai Galos <mihaigalos@gmail.com>
2022-12-03 09:13:42 +0100

    Service: First successful PoC

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::env;
 use dstat::colorizer::colorize;
+use dstat::service::systemctl_is_active;
 
 fn main() {
     let arguments: Vec<String> = env::args().collect();
-    colorize(arguments, systemctl_is_active);
+    let statuses = colorize(arguments, systemctl_is_active);
+    
+    for status in statuses {
+        print!("{} ", status);
+    }
+    println!("");
 }
 
--- src/service.rs
+++ src/service.rs
@@ -1,5 +1,15 @@
-pub fn systemctl_is_active(_service: &str) -> bool {
+pub fn systemctl_is_active(service: &str) -> bool {
+    use std::process::Command;
+    let output = Command::new("systemctl")
+        .args([
+            "is-active",
+            service,
+        ])
+        .output()
+        .expect(&format!("Unknown service {}.", service));
+
+    if output.status.code().unwrap() == 0 {
+        return true;
+    }
     false
 }
-
-
